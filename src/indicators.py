8c78322659e83607d70a72136a477a10e5c02595
shi7-7-7 <7n.turakiewicz.mateusz@gmail.com>
2026-07-23 21:44:26 +0200

    Created indicators architecture

--- src/indicators.py
+++ src/indicators.py
@@ -1,6 +1,35 @@
 import pandas as pd
 import numpy as np 
 
-class Calculate():
-    def __init__(self):
-        return None
+class Indicators:
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.df = df
+    
+    def SMA(self) -> float:
+        #Simple Moving Averate
+        return result
+
+    def EMA(self) -> float:
+        #Exponential Moving Average
+        return result
+
+    def MACD(self) -> float:
+        #Moving Average Convergence Divergence
+        return result
+
+    def Stochastic(self) -> float:
+        #Stochastic Oscillator
+        return result
+
+    def RSI(self) -> float:
+        #Relative Strength Index
+        return result
+
+    def ADX(self) -> float:
+        #Average Directional Index
+        return result
+
+    def OBV(self) -> float:
+        #On Balance Volume
+        return result
+
